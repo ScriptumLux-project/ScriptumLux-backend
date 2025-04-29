9f9a96c4e408a7f94c43ab5204f84184131147ec
CrashPine <grekovit7@gmail.com>
2025-04-29 02:50:29 -0700

    createdDirectories

--- ScriptumLux.API/obj/Debug/net9.0/ScriptumLux.API.AssemblyInfo.cs
+++ ScriptumLux.API/obj/Debug/net9.0/ScriptumLux.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ScriptumLux.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec99232492ed071be2003fc93a512f12418150d1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e3e7516111eff0bf222b88fc02f720b6f9fec5c")]
 [assembly: System.Reflection.AssemblyProductAttribute("ScriptumLux.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ScriptumLux.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
