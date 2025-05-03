0a407c53b0035ffa8e6ba22919a5bb2e30167369
CrashPine <grekovit7@gmail.com>
2025-05-03 07:50:38 -0700

    db creation

--- ScriptumLux.BLL/obj/Debug/net9.0/ScriptumLux.BLL.AssemblyInfo.cs
+++ ScriptumLux.BLL/obj/Debug/net9.0/ScriptumLux.BLL.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ScriptumLux.BLL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f9a96c4e408a7f94c43ab5204f84184131147ec")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a34475b7c75e372075418534874d86b08cec76f9")]
 [assembly: System.Reflection.AssemblyProductAttribute("ScriptumLux.BLL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ScriptumLux.BLL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
